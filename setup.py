86e316b41cd476cc2bd9ca3ae9506b821cd3f5c8
Pascual Tejero Cervera <Pascual.TejeroCervera@radboudumc.nl>
2026-07-01 13:47:46 +0200

    add setup.py to make installable with pip and editable with pip install -e .

---
+++ setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+setup(
+    install_requires=[
+        f"nnInteractive_v1 @ file://{here}/models/nninteractive_v1",
+        f"nnInteractive_v2 @ file://{here}/models/nninteractive_v2",
+        "surface-distance @ git+https://github.com/google-deepmind/surface-distance.git",
+        "numpy>=2.0",
+        "scipy>=1.10",
+        "scikit-image>=0.21",
+        "nibabel>=5.0",
+    ],
+)
