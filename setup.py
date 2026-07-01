de741df1e8d1e5a7dcd9ed9660c61e901b769dad
Pascual Tejero Cervera <Pascual.TejeroCervera@radboudumc.nl>
2026-07-01 14:15:18 +0200

    chore: add dependency of numpy<2.0 to setup.py and pyproject.toml

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
         f"nnInteractive_v1 @ file://{here}/models/nninteractive_v1",
         f"nnInteractive_v2 @ file://{here}/models/nninteractive_v2",
         "surface-distance @ git+https://github.com/google-deepmind/surface-distance.git",
-        "numpy>=2.0",
+        "numpy<2.0",
         "scipy>=1.10",
         "scikit-image>=0.21",
         "nibabel>=5.0",
